caf44ecf3899ffb50a349466936296f8e2cd6a87
Mustansir <mustansirmama@gmail.com>
2023-10-21 11:50:41 +0530

    Update contact us embed

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ export default function ContactPage() {
     return (
         <Layout description="Contact Us">
             <div className="items-center">
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd7P2KzpiugzXD6nb2MorhEuGgnqHC6dq976icN0XdUhP-KHg/viewform?embedded=true" width="640" height="1524" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+							<iframe src="https://tally.so/r/w47jKd?transparentBackground=1" width="640" height="1524" frameborder="0" marginheight="0" marginwidth="0" title="Welcome to OCEN 😁">Loading…</iframe>
             </div>
         </Layout>
     );
